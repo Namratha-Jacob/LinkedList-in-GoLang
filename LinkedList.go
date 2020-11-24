3ae13c85ac43a7933075b2822b3ebb8ea9943118
Namratha-Jacob <30745556+Namratha-Jacob@users.noreply.github.com>
2020-11-24 18:45:50 +0530

    added comments

--- LinkedList.go
+++ LinkedList.go
@@ -1,4 +1,4 @@
-//implementing stack using linked list
+/***** Implementing STACK using Linked list *******/
 
 package main
 
@@ -14,6 +14,7 @@ type LList struct {
 	length int
 }
 
+// Pushing nodes to the TOP of stack
 func (l *LList) pushStack(n *Node) {
 	second := l.head
 	l.head = n
@@ -31,10 +32,12 @@ func (l LList) displayStack() {
 	fmt.Printf("\n")
 }
 
+// Popping nodes from Top of stack
 func (l *LList) popStack() {
 	if l.length == 0 {
 		return
 	} else {
+		fmt.Println(l.head.data)
 		l.head = l.head.next
 		l.length--
 		return
@@ -43,11 +46,16 @@ func (l *LList) popStack() {
 
 func main() {
 	mylist := LList{}
+
+	fmt.Println("Creating 5 nodes")
+
 	node1 := &Node{data: 10}
 	node2 := &Node{data: 20}
 	node3 := &Node{data: 30}
 	node4 := &Node{data: 40}
 	node5 := &Node{data: 50}
+
+	fmt.Println("Pushing the 5 nodes into stack")
 
 	mylist.pushStack(node1)
 	mylist.pushStack(node2)
@@ -55,9 +63,15 @@ func main() {
 	mylist.pushStack(node4)
 	mylist.pushStack(node5)
 
+	fmt.Println("Displaying current stack:")
+
 	mylist.displayStack()
 
+	fmt.Println("Popping a node from stack:")
+
 	mylist.popStack()
+
+	fmt.Println("Displaying current stack: ")
 
 	mylist.displayStack()
 
